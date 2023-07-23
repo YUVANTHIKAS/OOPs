fad40eb433554747136683d4f53bfe7cf5597a2b
Yuvanthika Sarathy <yuvanthika.s@tvsnext.com>
2023-07-23 18:48:47 +0530

    [yuvanthika] Add:created

---
+++ OOPs/StockAccountManagement/StockOperation.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using OOPs.DataInventoryManagement;
+using OOPs.InventoryManagement;
+using System;
+using System.Collections.Generic;
+
+namespace OOPs.StockAccountManagement
+{
+    public class StockOperation
+    {
+        InventoryManagementDetails list;
+        public void ReadStockJson(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            List<StockDetails> list = JsonConvert.DeserializeObject<List<StockDetails>>(json);
+            foreach (var data in list)
+            {
+                Console.WriteLine(data.StockName + " " + data.NoOfShares + " " + data.SharePrice);
+            }
+        }
+
+
+    }
+}
